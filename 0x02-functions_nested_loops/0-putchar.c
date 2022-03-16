b776fafb459a21b8926bb9f4b06bc73b79754aa0
Ibrahim Taofeek <Ibraheem.Taofeek@fintraksoftware.com>
2022-03-16 17:36:23 +0100

    Nested Loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,23 @@
-#include "holberton.h"
+#include "main.h"
+
 
 /**
- * main - program that prints Holberton.
+ * main - Entry point
  *
- * Return: int
+ * Return: nothing, return void
  */
 
 int main(void)
 {
-	int n = 0;
-	char str_hbt[9] = "Holberton";
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 
-	while (n < 9)
-	{
-		_putchar(str_hbt[n]);
-		n += 1;
-	}
-	_putchar('\n');
-	return (0);
+return (0);
 }
